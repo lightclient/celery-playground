bc464b3a62a06febe51bb9218c3b33230b38a5fe
Matt Garnett <matt@mattgarnett.com>
2019-01-17 10:58:39 +0100

    add kubernetes deployment files

--- tasks/views.py
+++ tasks/views.py
@@ -4,11 +4,9 @@ from rest_framework.views import APIView
 from tasks.tasks import add
 
 class Add(APIView):
-    def get(self, request):
-        return JsonResponse({})
-
     def post(self, request):
         data = request.data
+        
         if "x" in data and "y" in data:
             add.delay(data["x"], data["y"])
             return JsonResponse({ "message": "success"})
